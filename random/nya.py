aa4a76227b523395443fb9eee68f478d0a694dcd
revlta <rrlukyanova@yandex.ru>
2024-09-26 19:55:48 +0300

    git commit

---
+++ random/nya.py
@@ -0,0 +1,38 @@
+#13.09.24 cw 1 and 2 tasks
+
+# print('Ваши фамилия, имя?')
+# name = input()
+# print('Сколько вам лет?')
+# age = input()
+# print('Где вы живете?')
+# placeofliving = input()
+
+# print(f'\n Ваше фамилия, имя {name} \n Ваш возраст {age} \n Вы живете в {placeofliving}')
+
+# import math as
+# x = int(input())
+# t = int(input())
+
+# z = round(((9 * math.pi * t + 10 * math.cos(x)) / (math.sqrt(t) - abs(math.sin(t)))) * (math.pow(math.e, x)), 2)
+# print(z)
+
+
+#1 hw
+# import math
+
+# r = int(input())
+# print(round(2 * math.pi * r, 2))
+# print(round(math.pi * math.pow(r, 2), 2))
+# #2 hw
+# x = 10
+# y = 55
+# print(x, y)
+# x, y = y, x
+# print(x, y)
+# #3 hw
+# L = int(input())
+# g = 9.81
+# print((2 * math.pi * math.sqrt(float(L) / g), 2))
+
+
+#practice lesson 2
