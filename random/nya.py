4d4be6f325ce57bdb397e9219991da6b5d7b8d86
revlta <rrlukyanova@yandex.ru>
2024-10-03 21:22:39 +0300

    git commit

--- random/nya.py
+++ random/nya.py
@@ -1,4 +1,4 @@
-#13.09.24 cw 1 and 2 tasks
+# 13.09.24 cw 1 and 2 tasks
 
 # print('Ваши фамилия, имя?')
 # name = input()
@@ -17,7 +17,7 @@
 # print(z)
 
 
-#1 hw
+# 1 hw
 # import math
 
 # r = int(input())
@@ -35,31 +35,74 @@
 # print((2 * math.pi * math.sqrt(float(L) / g), 2))
 
 
-#practice lesson 2
-#hw 1
+# practice lesson 2
+# hw 1
 # a = float(input())
 # b = float(input())
 # if b == 0:
 #     print('ты на ноль делишь дурак')
 # else:
 #     print(a / b)
-#hw 2
+# hw 2
 # summa = int(input())
 # if summa > 20:
 #     print(f'стоимость составила {round(summa - (summa * 0.35), 2)}, ваша скидка 35%')
 # else:
 #     print('денег мало скидки нет ха')
-#hw 3
-a = int(input())
-m = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
-if 1 <= a <= 12:
-    if 9 <= a < 12:
-        print(f'это {m[a]}, осень')
-    elif 3 <= a < 6:
-        print(f'это {m[a]}, весна')
-    elif 6 <= a < 9:
-        print(f'это {m[a]}, лето')
-    else:
-        print(f'это {m[a]}, зима')
-else:
-    print('такого месяца нет(')
+# hw 3
+# a = int(input())
+# m = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+# if 1 <= a <= 12:
+#     if 9 <= a < 12:
+#         print(f'это {m[a]}, осень')
+#     elif 3 <= a < 6:
+#         print(f'это {m[a]}, весна')
+#     elif 6 <= a < 9:
+#         print(f'это {m[a]}, лето')
+#     else:
+#         print(f'это {m[a]}, зима')
+# else:
+#     print('такого месяца нет(')
+
+# 27.09 cw
+
+# 1
+# n = float(input())
+# for i in range(1, 11):
+#   print(f'{i} килограмм конфет стоит {i * n}')
+# 2
+# sum = 0
+# num = 0
+# n = input().split()
+# for i in n:
+#     sum += int(i)
+#     num += 1
+# print(f'сумма {sum}, количество чисел {num}')
+# 3
+# n = [1, '2', 3, 4, '5', '!', 'FF', '5', '7!']
+# sum = 0
+# for i in n:
+#   if str(i).isdigit():
+#     sum += int(i)
+# print(sum)
+
+#1 slognaya
+# n = int(input())
+# ki = ['2', '3', '4']
+# for i in range(n + 1):
+#   if str(i)[-1] == '1' and i != 11:
+#     print(f'{i} овечка')
+#   elif str(i)[-1] in ki and not (11 < i < 15):
+#     print(f'{i} овечки')
+#   else:
+#     print(f'{i} овечек')
+
+#1 овечкА; 2-3-4 овечкИ; 5-6-7-8-9- овечЕК
+#2 slogNaya
+n =  ['Ab', 'AC', 'CD', 'bd', 'te', 'ed']
+lst = []
+for i in n:
+  for j in i:
+    if not j in lst:
+      lst.append(j)
+print(lst)
